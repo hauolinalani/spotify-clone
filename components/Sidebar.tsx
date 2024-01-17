6484c2e0b479c0e7cecd5021b427f09b22135bc2
Hau'olinalani O'Brien <ulapulelehua@gmail.com>
2024-01-17 13:02:32 -1000

    Updating/editing sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from 'next/navigation';
 import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
 import Box from './Box';
+import SidebarItem from './SidebarItem';
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -14,13 +15,13 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
 
   const routes = useMemo(() => [
     {
-        icon:HiHome,
+      icon: HiHome,
       label: 'home',
       active: pathname !== '/search',
       href: '/search',
     },
     {
-        icon: BiSearch,
+      icon: BiSearch,
       label: 'Search',
       active: pathname === '/search',
       href: '/search',
@@ -28,22 +29,37 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
   ], [pathname]);
 
   return (
-    <div className= "flex h-full">
-    <div
-    className="hidden
-    md:flex
-    flex-col
-    gap-y-2
-    bg-black 
-    h-full
-    w-[300px]
-    p-2
-    "
-    >  
-<Box>
-  Sidebar Navigation  
-</Box>
-    </div>
+    <div className="flex h-full">
+      <div 
+      className="
+      hidden
+       md:flex 
+       flex-col 
+       gap-y-2
+        bg-black 
+        h-full
+         w-[300px] 
+         p-2 
+         ">
+        <Box>
+          <div className="
+          flex 
+          flex-col
+           gap-y-4
+            px-5 
+            py-4">
+            {routes.map((item) => (
+              <SidebarItem
+                key={item.label}
+                {...item}
+              />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          Song Library
+        </Box>
+      </div>
     </div>
   );
 };
--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -1,10 +1,46 @@
-const SidebarItem = () => {
-return (
-    <div>
-Sidebar Item
+import React from 'react';
+import { IconType } from 'react-icons';
+import Link from 'next/link';
+import { twMerge } from 'tailwind-merge';
 
-    </div>
-);
-
+interface SidebarItemProps {
+  icon: IconType;
+  label: string;
+  active?: boolean;
+  href: string;
 }
-export default SidebarItem
+
+const SidebarItem: React.FC<SidebarItemProps> = ({ 
+    icon, 
+    label, 
+    active,
+     href }) => {
+  return (
+    <Link 
+    href={href} 
+    className={twMerge(`
+      flex
+      flex-row
+      h-auto
+      items-center
+      w-full
+      gap-x-4
+      text-md
+      font-medium
+      cursor-pointer
+      hover:text-white
+      transition
+      text-neutral-400
+      py-1
+      ${active && "text-white"}
+    
+    `)}
+    >
+        
+      Sidebar Item
+    </Link>
+  );
+};
+
+export default SidebarItem;
+
