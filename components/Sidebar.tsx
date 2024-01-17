40660b06cf850aecf19dde32c6c223e42c33af3a
Hau'olinalani O'Brien <ulapulelehua@gmail.com>
2024-01-17 13:45:18 -1000

    adding song library

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
 import Box from './Box';
 import SidebarItem from './SidebarItem';
+import Library from './Library';
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -36,7 +37,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
        md:flex 
        flex-col 
        gap-y-2
-        bg-gray-500
+        bg-black
         h-full
          w-[300px] 
          p-2 
@@ -57,9 +58,12 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
           </div>
         </Box>
         <Box className="overflow-y-auto h-full">
-          Song Library
+         <Library/>
         </Box>
-      </div>
+      </div>{children}
+      <main className="h-full flex-1 overflow-y-auto py-2">
+      {children}
+      </main>
     </div>
   );
 };
