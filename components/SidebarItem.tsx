c34613848242646f93175399442858e7b1bb4ad0
Hau'olinalani O'Brien <ulapulelehua@gmail.com>
2024-01-17 14:20:52 -1000

    Add button comp

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { IconType } from 'react-icons';
 import Link from 'next/link';
+import { IconType } from 'react-icons';
 import { twMerge } from 'tailwind-merge';
 
 interface SidebarItemProps {
@@ -10,38 +9,38 @@ interface SidebarItemProps {
   href: string;
 }
 
-const SidebarItem: React.FC<SidebarItemProps> = ({ 
-    icon: Icon,
-    label, 
-    active,
-     href }) => {
-  return (
-    <Link 
-    href={href} 
-    className={twMerge(`
-      flex
-      flex-row
-      h-auto
-      items-center
-      w-full
-      gap-x-4
-      text-md
-      font-medium
-      cursor-pointer
-      hover:text-white
-      transition
-      text-neutral-400
-      py-1
-      ${active && "text-white"}
-    
-    `)}
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  icon: Icon,
+  label,
+  active,
+  href
+}) => {
+  return ( 
+    <Link
+      href={href} 
+      className={twMerge(`
+        flex 
+        flex-row 
+        h-auto 
+        items-center 
+        w-full 
+        gap-x-4 
+        text-md 
+        font-medium
+        cursor-pointer
+        hover:text-white
+        transition
+        text-neutral-400
+        py-1`,
+        active && "text-white"
+        )
+      }
     >
-        
-      <Icon size={26}/>
-      <p className="truncate w-full">{label}</p>
+      <Icon size={26} />
+      <p className="truncate w-100">{label}</p>
     </Link>
-  );
-};
+   );
+}
 
 export default SidebarItem;
 
